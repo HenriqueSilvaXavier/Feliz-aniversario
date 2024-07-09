29925dabc636fa3387de0b0d5a16fab328a55414
HenriqueSilvaXavier <134643923+HenriqueSilvaXavier@users.noreply.github.com>
2024-07-09 12:09:05 -0300

    Update Feliz aniversário.py

--- Feliz aniversário.py
+++ Feliz aniversário.py
@@ -1,31 +1,25 @@
 from datetime import date
-atual="{}".format(date.today())
-atual=atual.replace("-", " ")
-atual=atual.split()
-for k in range(0, len(atual)):
-	atual[k]=int(atual[k])
-print(atual)
-n=input("Digite sua data de nascimento: ")
-n=n.replace("/", " ")
-n=n.split()
-n=n[::-1]
-for k in range(0, len(n)):
-	n[k]=int(n[k])
-print(n)
-def tempo(n):
-	if atual[1]<n[1]:
-		return True
-	elif atual[1]==n[1]:
-		if atual[2]<n[2]:
-			return True
-		elif atual[2]>n[2]:
-		    return False
-	elif atual[1]>n[1]:
-		return False
-if n[1]==atual[1] and n[2]==atual[2]:
-	print("Feliz aniversário!")
-i=atual[0]-n[0]-1
-tempo(n)
-if tempo(n)==True:
-	i=i+1
-print("Você tem {} anos".format(i))
+
+# Obtendo a data atual
+atual = date.today()
+atual_lista = [atual.year, atual.month, atual.day]
+print(atual_lista)
+
+# Solicitando a data de nascimento do usuário
+nascimento = input("Digite sua data de nascimento (dd/mm/yyyy): ")
+nascimento = nascimento.replace("/", " ").split()[::-1]
+nascimento = [int(i) for i in nascimento]
+print(nascimento)
+
+def tempo(data_atual, data_nascimento):
+    if (data_atual[1], data_atual[2]) < (data_nascimento[1], data_nascimento[2]):
+        return data_atual[0] - data_nascimento[0] - 1
+    else:
+        return data_atual[0] - data_nascimento[0]
+
+# Verifica se hoje é aniversário
+if atual_lista[1] == nascimento[1] and atual_lista[2] == nascimento[2]:
+    print("Feliz aniversário!")
+
+idade = tempo(atual_lista, nascimento)
+print("Você tem {} anos".format(idade))
